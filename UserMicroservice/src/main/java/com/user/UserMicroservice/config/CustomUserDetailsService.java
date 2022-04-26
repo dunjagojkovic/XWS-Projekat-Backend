bd183b47b703293ef2d60b6f7bd065ff2338cf22
Milica Urukalo <74359178+milica38@users.noreply.github.com>
2022-04-27 00:15:11 +0200

    Security and config

---
+++ UserMicroservice/src/main/java/com/user/UserMicroservice/config/CustomUserDetailsService.java
@@ -0,0 +1,58 @@
+package com.user.UserMicroservice.config;
+
+import com.user.UserMicroservice.model.User;
+import com.user.UserMicroservice.repository.UserRepository;
+import com.user.UserMicroservice.security.Authority;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> user = userRepository.findByUsername(username);
+        org.springframework.security.core.userdetails.User.UserBuilder builder = null;
+
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("USERNAME_NOT_FOUND");
+        }
+
+        builder = org.springframework.security.core.userdetails.User.withUsername(username);
+        String password = user.get().getPassword();
+
+        builder.password(password);
+
+        return builder.build();
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthoritiesFromUserDetails(UserDetails principle) {
+
+        List<Authority> authorities = new ArrayList<>();
+
+        Optional<User> user = userRepository.findByUsername(principle.getUsername());
+
+        if(!user.isPresent()) {
+            return authorities;
+        }
+
+        Authority authority = new Authority();
+        authorities.add(authority);
+        return authorities;
+    }
+
+    public User findByUsername(String username) { return userRepository.findByUsername(username).get(); }
+}
---
+++ UserMicroservice/src/main/java/com/user/UserMicroservice/security/Authority.java
@@ -0,0 +1,18 @@
+package com.user.UserMicroservice.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+
+public class Authority implements GrantedAuthority {
+    String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() { return name; }
+}
