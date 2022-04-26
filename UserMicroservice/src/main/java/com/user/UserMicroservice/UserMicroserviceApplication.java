39fac85b2ce20bb6770cf1760f735d4e5acdfcdb
Dunja Gojković <74664594+dunjagojkovic@users.noreply.github.com>
2022-04-26 13:49:33 +0200

    initial spring project created

---
+++ UserMicroservice/src/main/java/com/user/UserMicroservice/UserMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.user.UserMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class UserMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMicroserviceApplication.class, args);
+	}
+
+}
