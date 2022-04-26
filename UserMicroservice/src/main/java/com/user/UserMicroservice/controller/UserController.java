0ee882afcdb5cbf16f8baaf60d7e6e7612e76bac
Milica Urukalo <74359178+milica38@users.noreply.github.com>
2022-04-27 00:15:29 +0200

    User login

--- UserMicroservice/src/main/java/com/user/UserMicroservice/controller/UserController.java
+++ UserMicroservice/src/main/java/com/user/UserMicroservice/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.user.UserMicroservice.controller;
 
+import com.user.UserMicroservice.config.CustomUserDetailsService;
+import com.user.UserMicroservice.dto.LoginDTO;
+import com.user.UserMicroservice.dto.LoginResponseDTO;
 import com.user.UserMicroservice.dto.RegistrationDTO;
 import com.user.UserMicroservice.model.User;
+import com.user.UserMicroservice.security.TokenUtil;
 import com.user.UserMicroservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +21,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private TokenUtil tokenUtils;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private CustomUserDetailsService customUserService;
+
     @PostMapping(consumes = "application/json", path = "/register")
     public ResponseEntity<?> registerClient(@RequestBody RegistrationDTO registrationDTO) {
 
@@ -27,5 +40,20 @@ public class UserController {
         }
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
+
+        User user = customUserService.findByUsername(loginDTO.getUsername());
+
+        if (user == null || !passwordEncoder.matches(loginDTO.getPassword(), user.getPassword()) || !loginDTO.getUsername().equals(user.getUsername())) {
+            return  ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
+        }
+
+        String token = tokenUtils.generateToken(user.getUsername());
+        LoginResponseDTO responseDTO = new LoginResponseDTO();
+        responseDTO.setToken(token);
+        return ResponseEntity.ok(responseDTO);
     }
 }
---
+++ UserMicroservice/src/main/java/com/user/UserMicroservice/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.user.UserMicroservice.dto;
+
+public class LoginResponseDTO {
+
+    private String token;
+
+    public String getToken(){
+        return token;
+    }
+    public void setToken(String token){
+        this.token = token;
+    }
+}
