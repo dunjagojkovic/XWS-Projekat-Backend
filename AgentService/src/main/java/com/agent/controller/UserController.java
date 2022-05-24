b83588d66633e978009146b093067c98ac9c6111
Milica Urukalo <74359178+milica38@users.noreply.github.com>
2022-05-24 17:17:15 +0200

    Login for users

--- AgentService/src/main/java/com/agent/controller/UserController.java
+++ AgentService/src/main/java/com/agent/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.agent.controller;
 
+import com.agent.config.CustomUserDetailsService;
+import com.agent.dto.LoginDTO;
+import com.agent.dto.LoginResponseDTO;
 import com.agent.dto.RegistrationDTO;
 import com.agent.model.User;
+import com.agent.security.TokenUtil;
 import com.agent.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,6 +27,13 @@ public class UserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private TokenUtil tokenUtils;
+
+    @Autowired
+    private CustomUserDetailsService customUserService;
+
+
     @PostMapping(consumes = "application/json", path = "/register")
     public ResponseEntity<?> registerUser(@RequestBody RegistrationDTO registrationDTO) {
 
@@ -33,6 +44,26 @@ public class UserController {
         }
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
+
+        User user = customUserService.findUserByUsername(loginDTO.getUsername());
+
+        if (user == null || !passwordEncoder.matches(loginDTO.getPassword(), user.getPassword()) || !loginDTO.getUsername().equals(user.getUsername())) {
+            return  ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
+        }
+
+        String token = tokenUtils.generateToken(user.getUsername());
+        LoginResponseDTO responseDTO = new LoginResponseDTO();
+        responseDTO.setToken(token);
+        return ResponseEntity.ok(responseDTO);
+    }
+    @GetMapping(path = "/current")
+    public ResponseEntity<?> getCurrentUser() {
+
+        return new ResponseEntity<>(userService.getCurrentUser(), HttpStatus.OK);
     }
 
 }
--- AgentService/src/main/java/com/agent/service/UserService.java
+++ AgentService/src/main/java/com/agent/service/UserService.java
@@ -1,5 +1,6 @@
 package com.agent.service;
 
+import com.agent.config.SecurityUtils;
 import com.agent.dto.RegistrationDTO;
 import com.agent.model.User;
 import com.agent.repository.UserRepository;
@@ -41,6 +42,23 @@ public class UserService {
         user.setType("User");
 
         return userRepository.save(user);
+    }
+
+    public User getCurrentUser() {
+
+        String username = SecurityUtils.getCurrentUserLogin().get();
+        System.out.println(username);
+        return userRepository.findByUsername(username).get();
+    }
+
+    public boolean userExists(User user, List<User> users) {
+
+        for(User u: users){
+            if(u.getId().equals(user.getId())){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
