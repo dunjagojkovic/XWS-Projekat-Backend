f8f22af071ce7019db169d037c52879b015a7c2a
Dunja Gojković <74664594+dunjagojkovic@users.noreply.github.com>
2022-06-05 21:20:27 +0200

    generate random key for job offers

--- AgentService/src/main/java/com/agent/controller/UserController.java
+++ AgentService/src/main/java/com/agent/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -58,6 +59,9 @@ public class UserController {
         String token = tokenUtils.generateToken(user.getUsername());
         LoginResponseDTO responseDTO = new LoginResponseDTO();
         responseDTO.setToken(token);
+        String key = UUID.randomUUID().toString();
+        responseDTO.setKey(key);
+
         return ResponseEntity.ok(responseDTO);
     }
     @GetMapping(path = "/current")
