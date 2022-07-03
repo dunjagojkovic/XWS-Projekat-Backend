a0e07965b96c97f1be5601c5ec4eec3080ca40bf
Sladjana Colakovic <74436050+SladjanaColakovic@users.noreply.github.com>
2022-07-03 02:03:00 +0200

    usersById fix

--- MicroservicesProject/user_service/repository/userStore.go
+++ MicroservicesProject/user_service/repository/userStore.go
@@ -100,9 +100,6 @@ func (store *UserStore) GetUsersById(usersById []string) ([]*model.User, error) 
 	fmt.Println(usersById)
 	filter := bson.D{{}}
 	result, err := store.filter(filter)
-	for _, r := range result {
-		fmt.Println(r.Id)
-	}
 	var users []*model.User
 
 	if len(usersById) == 0 {
@@ -114,8 +111,10 @@ func (store *UserStore) GetUsersById(usersById []string) ([]*model.User, error) 
 	}
 	for _, user := range result {
 		for _, userById := range usersById {
-			if user.Id.String() == userById {
-				fmt.Println(user.Id.String())
+			objID, _ := primitive.ObjectIDFromHex(userById)
+			fmt.Println(user.Id)
+			if user.Id == objID {
+				fmt.Println(user.Id)
 				users = append(users, user)
 
 			}
